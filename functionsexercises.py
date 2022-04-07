9ccf51eb76e7498cd36ed7d373e26bc61d7b5494
Michael Nguyen <michael263286@gmail.com>
2022-04-06 20:26:27 -0500

    finished

--- functionsexercises.py
+++ functionsexercises.py
@@ -9,19 +9,43 @@
 #Call your function after you have defined it and pass in two strings. Do this twice.
 
 
+from stat import FILE_ATTRIBUTE_SYSTEM
+
+
 def my_name(first_name, last_name):
   print("Top of the morning to you," + " " + first_name + " " + last_name + "!")
 
 
-my_name("Michael", "Nguyen")
-my_name("John", "Doe")
+#my_name("Michael", "Nguyen")
+#my_name("John", "Doe")
 
 
 def email(first_name , last_name, domain):
   print(first_name[0].lower() + "." + last_name + domain)
 
-email("Michael","Nguyen","gmail.com")
-email("Joe","Doe"," gmail.com")
+#email("Michael","Nguyen","gmail.com")
+#email("Joe","Doe"," gmail.com")
+
+def username(first_name,last_name,):
+  print(first_name[0:3].lower() + "_" + last_name[0:5].lower())
+
+#username("Michael", "Nguyen")
+#username("John", "Doe")
+
+def contact(first_name, last_name, domain):
+  print(f"Welcome to E Corp, {first_name} {last_name}")
+  print("Email: ")
+  email("Michael", "Nguyen", "domain")
+  print ("Username: ")
+  username("Michael", "Nguyen")
+
+contact("Michael", "Nguyen", "gmail.com")
+
+
+
+
+
+
 
 
 
