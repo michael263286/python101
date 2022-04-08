6c8e45d6b64d69c76a00cc979df79a84df0cfa5b
Michael Nguyen <michael263286@gmail.com>
2022-04-08 09:49:16 -0500

    homework update

--- HomeworkPython101.py
+++ HomeworkPython101.py
@@ -32,7 +32,7 @@
 # Write a function called group_tip_calculator that asks the user for the bill total, the tip percentage, and the number of people. Pass the total and percentage values to the percentage_plus function and then divide the result by the number of people in the group. Print the total including the tip as well as the cost per person.
 
 
-def percentageplus(total,tip,people):
+def percentage_plus(total,tip,people):
   tip_amount = total * (tip/100)
   return (total + tip_amount)
 
@@ -44,9 +44,9 @@ people = int(input("How many people are in the group? "))
 def group_tip_calculator(total_bill):
   return(total_bill)
 
-total_bill = percentageplus(total,tip,people)
+total_bill = percentage_plus(total,tip,people)
 
 group_tip_calculator("total_bill")
 
 print(f"Total bill is ${total_bill}")
-print(f"Each person should pay ${total_bill/people}")
+print(f"Each person should pay ${total_bill/people:.2f}")
