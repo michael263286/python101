d224bc9f910b32f26d59dc042bc43e5190864332
Michael Nguyen <michael263286@gmail.com>
2022-04-12 08:47:16 -0500

    todolist

---
+++ Homeworkpython101/TodolistHW.py
@@ -0,0 +1,22 @@
+
+
+tasks = {}
+
+tasks = {
+  "title": " ",
+  "priority": " "
+}
+
+
+
+user_input = input("Press 1 to add task \nPress 2 to delete task \nPress 3 to view all tasks \nPress q to quit")
+
+if user_input == 1:
+  tasks["title"]=print(input("Add task: "))
+  tasks["priority"]=print(input("What's the priority?: "))
+
+print(tasks)
+
+
+
+
